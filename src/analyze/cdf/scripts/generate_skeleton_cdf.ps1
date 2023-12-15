d3ef8110261eb1d4c129e484720968a4e061de79
mfacchinelli <m.facchinelli@imperial.ac.uk>
2023-12-15 11:18:54 +0000

    Remove CDF files

--- src/analyze/cdf/scripts/generate_skeleton_cdf.ps1
+++
@@ -1,4 +0,0 @@
-$DIR=Get-Location
-
-skeletoncdf.exe $DIR\..\skeleton\imap_L1b_mago_skeletontable_V01.skt -cdf $DIR\..\imap_L1b_mago_skeletontable_V01.cdf
-skeletoncdf.exe $DIR\..\skeleton\imap_L1b_magi_skeletontable_V01.skt -cdf $DIR\..\imap_L1b_magi_skeletontable_V01.cdf
