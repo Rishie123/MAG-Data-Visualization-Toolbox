aeaaca999d74888dc1df08095a452f03334207c7
mfacchinelli <m.facchinelli@imperial.ac.uk>
2024-01-09 14:42:06 +0000

    Fix save figure issues

--- resources/ReleaseNotes.md
+++ resources/ReleaseNotes.md
@@ -6,6 +6,8 @@
 - Only filter one vector for mode changes, and one second for range changes
 - Hide unnecessary constants in meta data classes
 - Make sure custom events have padded y-axis limits
+- Fix issue with saving data with colon in name
+- Fix issue with saving too large figures causing an error
 - Fix issue with consecutive figures not opening maximized
 - Fix issue with saving figures in folder names with dots
 - Fix small typo in HK data loading
--- src/visualize/+mag/+graphics/savePlots.m
+++ src/visualize/+mag/+graphics/savePlots.m
@@ -5,15 +5,22 @@ function savePlots(figures, location, options)
         figures (1, :) matlab.ui.Figure
         location (1, 1) string {mustBeFolder} = "results"
         options.DotReplacement (1, 1) string = "_"
+        options.ColonReplacement (1, 1) string = ""
     end
 
     for f = figures
 
         if isvalid(f)
 
-            name = fullfile(location, replace(f.Name, ".", options.DotReplacement));
+            name = replace(f.Name, [".", ":"], [options.DotReplacement, options.ColonReplacement]);
+            name = fullfile(location, name);
 
-            savefig(f, name);
+            try
+                savefig(f, name);
+            catch exception
+                warning("Could not save figure ""%s"":\n%s", f.Name, exception.message);
+            end
+
             exportgraphics(f, fullfile(name + ".png"), Resolution = 300);
         end
     end
