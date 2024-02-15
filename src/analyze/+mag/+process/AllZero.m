974fbbf58af8329743929e848a403be7b3a60cda
mfacchinelli <m.facchinelli@imperial.ac.uk>
2024-02-15 17:41:15 +0000

    Add step to remove vectors where time and selected variables are zero

---
+++ src/analyze/+mag/+process/AllZero.m
@@ -0,0 +1,52 @@
+classdef AllZero < mag.process.Step
+% ALLZERO Remove vectors where timestamp and data is all zero.
+
+    properties (Dependent)
+        Name
+        Description
+        DetailedDescription
+    end
+
+    properties
+        % VARIABLES Variables to check for all-zero.
+        Variables (1, :) string
+    end
+
+    methods
+
+        function this = AllZero(options)
+
+            arguments
+                options.?mag.process.AllZero
+            end
+
+            this.assignProperties(options);
+        end
+
+        function value = get.Name(~)
+            value = "All-Zero";
+        end
+
+        function value = get.Description(~)
+            value = "Remove vectors where timestamp and data is all zero.";
+        end
+
+        function value = get.DetailedDescription(this)
+            value = this.Description;
+        end
+
+        function data = apply(this, data, ~)
+
+            arguments
+                this (1, 1) mag.process.AllZero
+                data timetable
+                ~
+            end
+
+            locTime = data.Properties.RowTimes <= datetime(mag.process.DateTime.Epoch, ConvertFrom = "posix", TimeZone = "UTC");
+            locData = all(data{:, this.Variables} == 0, 2);
+
+            data(locTime & locData, :) = [];
+        end
+    end
+end
