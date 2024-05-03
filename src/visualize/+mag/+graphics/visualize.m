6d85287e51f3bbebc36ba780e56c2b59f90489b6
mfacchinelli <m.facchinelli@imperial.ac.uk>
2024-05-03 15:22:58 +0100

    Fix bug in rewriting of `mag.graphics.visualize`

--- src/visualize/+mag/+graphics/visualize.m
+++ src/visualize/+mag/+graphics/visualize.m
@@ -2,7 +2,7 @@ function varargout = visualize(varargin)
 % VISUALIZE Plot data with specified styles and options.
 
     try
-        [varargout(1:nargout)] = mag.graphics.factory.DefaultFactory().assemble(varargin{:});
+        [varargout{1:nargout}] = mag.graphics.factory.DefaultFactory().assemble(varargin{:});
     catch exception
         throwAsCaller(exception);
     end
