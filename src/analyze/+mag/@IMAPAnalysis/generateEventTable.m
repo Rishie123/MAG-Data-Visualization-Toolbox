5346fee1885d3e968dcafb44b56e25dc6ffdff2e
mfacchinelli <m.facchinelli@imperial.ac.uk>
2024-05-08 15:52:47 +0100

    Fix issue when ranges are empty

--- src/analyze/+mag/@IMAPAnalysis/generateEventTable.m
+++ src/analyze/+mag/@IMAPAnalysis/generateEventTable.m
@@ -209,10 +209,13 @@ function [rangeTable, events] = findRangeChanges(ranges, events, sensorName)
 
     % If the first range change is after the first event (and they are
     % close to each other) update its range.
-    firstRange = events(withtol(rangeTable.Time(1), seconds(0.5)), :);
+    if ~isempty(rangeTable)
 
-    if ~isempty(firstRange) && ismissing(firstRange.Range) && ~contains(firstRange.Label, "Range")
-        events{withtol(rangeTable.Time(1), seconds(0.5)), "Range"} = rangeTable.Range(1);
+        firstRange = events(withtol(rangeTable.Time(1), seconds(0.5)), :);
+
+        if ~isempty(firstRange) && ismissing(firstRange.Range) && ~contains(firstRange.Label, "Range")
+            events{withtol(rangeTable.Time(1), seconds(0.5)), "Range"} = rangeTable.Range(1);
+        end
     end
 
     % Complete automatic range changes.
